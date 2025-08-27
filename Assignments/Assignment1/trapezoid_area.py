9c1b1fa6b98eaf95d4716e32f04232b76beb5532
ColinHask <colinhaskins2022@gmail.com>
2025-08-27 19:16:57 -0400

    added trapezoid calculator

---
+++ Assignments/Assignment1/trapezoid_area.py
@@ -0,0 +1,12 @@
+while True:
+    try:
+        print("--- Trapezoid Area Calculator ---\n")
+        b1 = int(input("Enter base 1 length: "))
+        b2 = int(input("Enter base 2 length: "))
+        h = int(input("Enter length: "))
+
+        #area formula
+        a = ((b1+b2)/2)*h
+        print(f"Area: {a}")
+    except:
+        print(f"Invalid Input, please try again.")
