63b3bed087349d01b5f5ab01514c430cc7821ce7
ColinHask <colinhaskins2022@gmail.com>
2025-08-29 18:48:33 -0400

    spelling fix

--- Assignments/Assignment1/trapezoid_area.py
+++ Assignments/Assignment1/trapezoid_area.py
@@ -3,7 +3,7 @@ while True:
         print("--- Trapezoid Area Calculator ---\n")
         b1 = int(input("Enter base 1 length: "))
         b2 = int(input("Enter base 2 length: "))
-        h = int(input("Enter length: "))
+        h = int(input("Enter height: "))
 
         #area formula
         a = ((b1+b2)/2)*h
